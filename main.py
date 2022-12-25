8bfbcd184f42000da6c535f5b08897ee869f5de9
ber2minsin <ber2minsin@gmail.com>
2022-12-25 20:39:54 +0300

    Refactored utils.py size window

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from utils import key_down, screenshot, get_active_window_title
+from utils import key_down, screenshot, get_active_window_title, get_window_size
 import pydirectinput as pdi
 import time
 import pyautogui as pag
 import os
+import keyboard
+import cv2
+import numpy as np
+
 
 if __name__ == "__main__":
-    title = get_active_window_title()
-    screenshot(save_path=f"{os.getcwd()}/test.png", win_title=title)
-    key_down(["t", "e", "s", "t"])
-    pass
+    # test_title = get_active_window_title()
+    test_title = "illet"
+    key_down([x for x in "test"], delay=1)
+
+    cv2.namedWindow("Capture", cv2.WINDOW_NORMAL)
+
+
+    # Doing this in every loop probably would slow the capture, so we dont support resizes
+
+    size = get_window_size(test_title)
+    cv2.resizeWindow("Capture", size[2], size[3])
+    while not keyboard.is_pressed("f8"):
+
+        img = screenshot(save_path=f"{os.getcwd()}/test.png", win_title=test_title)
+        npimg = np.array(img)
+        npimg = cv2.cvtColor(npimg, cv2.COLOR_BGR2RGB)
+
+        cv2.imshow("Capture", npimg)
+
+        key = cv2.waitKey(1)
+
+
+# cv2.destroyWindow("Capture")
+
+
--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ import win32gui
 import keyboard
 import pyperclip
 from PIL import ImageGrab
+import timeit
 
 def key_down(key, delay=0.03):
     """Convenience function to press a list of keys \n
@@ -20,29 +21,44 @@ def key_down(key, delay=0.03):
         time.sleep(delay)
 
 
+
 def screenshot(save_path=None, win_title=None):
     """Convenience function to take a screenshot of the window with the given title\n"""
     if win_title:
-        x, y, w, h = find_window_movetop(win_title)
+        maximize_window(win_title)
+        x, y, w, h = get_window_size(win_title)
         img = ImageGrab.grab(bbox=(x, y, x+w, y+h))
         if save_path:
             img.save(save_path)
+        return img
     else:
         pag.screenshot(save_path)
 
-def find_window_movetop(win_title):
+def maximize_window(win_title):
     """This function finds the window with the given class name and moves it to the top\n
        `return` the window's position and size `(x, y, w, h)`"""
     hwnd = win32gui.FindWindow(None, win_title)
     win32gui.ShowWindow(hwnd,5)
     win32gui.SetForegroundWindow(hwnd)
+
+
+def get_window_size(win_title):
+    hwnd = win32gui.FindWindow(None, win_title)
+
+    # Get the window rect in screen coordinates
     window_rect = win32gui.GetWindowRect(hwnd)
+
+    # Get the client rect in client coordinates
     client_rect = win32gui.GetClientRect(hwnd)
-    region = x, y, w, h = window_rect[0], window_rect[1], client_rect[2], client_rect[3]
 
-    print(region)
-    return region
+    # Convert the client rect to screen coordinates
+    left, top = win32gui.ClientToScreen(hwnd, (0, 0))
+    right, bottom = win32gui.ClientToScreen(hwnd, (client_rect[2], client_rect[3]))
 
+    # Calculate the window dimensions
+    x, y, w, h = left, top, right - left, bottom - top
+
+    return x, y, w, h
 
 def get_active_window_title():
     """ This is an infinite loop that helps you get the title \n
@@ -53,5 +69,3 @@ def get_active_window_title():
             print(title)
             pyperclip.copy(title)
             return title
-
-
